7c5ef32c77a1861659e70ef4896ec040cb3205c4
Marauder22 <147262911+Marauder22@users.noreply.github.com>
2024-09-18 17:16:27 +0300

    main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,6 @@ fun main() {
         header(2) { "Список новостей" }
         text { "Сегодня мы изучаем работу с API KudaGo." }
 
-        // Пример использования функций
         val newsList = getNews()
         newsList.forEach { news ->
             text { "${news.title} - ${news.siteUrl}" }
