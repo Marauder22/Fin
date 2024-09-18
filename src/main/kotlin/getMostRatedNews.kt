3e0fbd412c44f29f9e0b1d60beb159507e8a618e
Marauder22 <147262911+Marauder22@users.noreply.github.com>
2024-09-18 17:14:53 +0300

    Update getMostRatedNews.kt

--- src/main/kotlin/getMostRatedNews.kt
+++ src/main/kotlin/getMostRatedNews.kt
@@ -2,9 +2,7 @@ import java.time.LocalDate
 
 fun List<News>.getMostRatedNews(count: Int, period: ClosedRange<LocalDate>): List<News> {
     return this.filter { news ->
-        // Предполагается, что поле publicationDate доступно в API
-        // news.publicationDate in period
-        true // Замените на реальную проверку
+        true 
     }.sortedByDescending { it.rating }
         .take(count)
 }
