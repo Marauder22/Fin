29d1876889344aee6ea6d8d3b4220ca422fe450b
Marauder22 <147262911+Marauder22@users.noreply.github.com>
2024-09-23 17:35:59 +0300

    Update getNews.kt

--- main/kotlin/getNews.kt
+++ main/kotlin/getNews.kt
@@ -1,14 +1,37 @@
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonDecodingException
 
-suspend fun getNews(count: Int = 100): List<News> {
+@Serializable
+data class News(
+    val id: Int,
+    val title: String,
+    val publishedAt: String,
+)
+
+object HttpClientProvider {
     val client = HttpClient()
-    val response: HttpResponse = client.get("https://api.kudago.com/v1/news/?page=1&page_size=$count&order_by=published_at&location=spb")
-    val jsonString = response.bodyAsText()
-    val newsList = Json.decodeFromString<List<News>>(jsonString) // Десериализация JSON в список новостей
-    client.close()
-    return newsList
+}
+suspend fun getNews(count: Int = 100): Result<List<News>> {
+    return try {
+        val response: HttpResponse = HttpClientProvider.client.get("https://api.kudago.com/v1/news/?page=1&page_size=$count&order_by=published_at&location=spb") {
+            headers {
+                append(HttpHeaders.Accept, ContentType.Application.Json.toString())
+            }
+        }
+        if (response.status.isSuccess()) {
+            val jsonString = response.bodyAsText()
+            val newsList = Json.decodeFromString<List<News>>(jsonString)
+            Result.success(newsList)
+        } else {
+            Result.failure(Exception("Ошибка HTTP: ${response.status}"))
+        }
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
 }
