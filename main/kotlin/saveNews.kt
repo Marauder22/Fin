1995b085e247682ce30f3a14d9b956e68b8cf0ca
Marauder22 <147262911+Marauder22@users.noreply.github.com>
2024-09-23 17:42:01 +0300

    Update saveNews.kt

--- main/kotlin/saveNews.kt
+++ main/kotlin/saveNews.kt
@@ -1,15 +1,34 @@
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import java.io.File
+import java.io.IOException
 
 fun saveNews(path: String, news: Collection<News>) {
     val file = File(path)
+
     if (file.exists()) {
         throw IllegalArgumentException("Файл уже существует: $path")
     }
+    try {
+  
+        csvWriter().open(file) {
+            writeRow("id", "title", "place", "description", "siteUrl", "favoritesCount", "commentsCount", "publishedAt")
 
-    file.printWriter().use { out ->
-        out.println("id,title,place,description,siteUrl,favoritesCount,commentsCount,publishedAt")
-        news.forEach { newsItem ->
-            out.println("${newsItem.id},${newsItem.title},${newsItem.place},${newsItem.description},${newsItem.siteUrl},${newsItem.favoritesCount},${newsItem.commentsCount},${newsItem.publishedAt}")
+            news.forEach { newsItem ->
+                writeRow(
+                    newsItem.id,
+                    newsItem.title,
+                    newsItem.place,
+                    newsItem.description,
+                    newsItem.siteUrl,
+                    newsItem.favoritesCount,
+                    newsItem.commentsCount,
+                    newsItem.publishedAt
+                )
+            }
         }
+    } catch (e: IOException) {
+        throw RuntimeException("Ошибка при записи в файл: ${e.message}", e)
+    } catch (e: Exception) {
+        throw RuntimeException("Неизвестная ошибка: ${e.message}", e)
     }
 }
